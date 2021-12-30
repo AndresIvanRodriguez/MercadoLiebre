5257d8f5865bd1a73f7cf66d6ee7d001a329c7a9
AndresIvanRodriguez <94801400+AndresIvanRodriguez@users.noreply.github.com>
2021-12-29 23:17:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.static(publicPath));
 app.listen(process.env.PORT || 3000,function() {
     console.log("Servidor corriendo correctamente en puerto 3000");
 })
-app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'/views/home.html')));
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'/views/home.html')));
 app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,'/views/login.html')));
 app.get('/register',(req,res)=> res.sendFile(path.join(__dirname,'/views/register.html')));
