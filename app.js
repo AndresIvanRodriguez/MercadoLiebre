a20a995e83f7f29e248b3acf035f0bb50a6a7ba8
AndresIvanRodriguez <94801400+AndresIvanRodriguez@users.noreply.github.com>
2021-12-29 22:56:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path= require('path');
 const publicPath=path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
-app.listen(process.env.PORT ||3130, ()=>console.log('servidor corriendo'));
+app.listen(process.env.PORT ||3130, function(){console.log('servidor corriendo')});
 
 app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'/views/home.html')));
 app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,'/views/login.html')));
